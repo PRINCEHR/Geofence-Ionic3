5e8e92e39c1866e4efced32ab79fb843e2d552a8
Prince <prince.hri999@gmail.com>
2018-05-07 16:38:51 +0530

    IONIC 3 Geolocation App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { GeofenceDetailsPage } from "../pages/geofence-details/geofence-details";
+import { GeofenceListItem } from "../components/geofence-list-item/geofence-list-item";
+import { GeofenceListPage } from "../pages/geofence-list/geofence-list";
+import { GeofenceService } from "../services/geofence-service";
+// import { HomePage } from '../pages/home/home';
+
+// const components = [
+//   MyApp,
+//   GeofenceDetailsPage,
+//   GeofenceListPage,
+//   GeofenceListItem
+// ]
+
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    
+    GeofenceDetailsPage,
+    GeofenceListPage,
+    GeofenceListItem
+   
   ],
   imports: [
     BrowserModule,
@@ -19,12 +38,19 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+   
+    GeofenceDetailsPage,
+    GeofenceListPage,
+    GeofenceListItem
+  
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+       
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+        GeofenceService
+
   ]
 })
 export class AppModule {}
